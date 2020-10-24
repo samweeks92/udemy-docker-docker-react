ec5dd8c4c9dba22ed01875c8c778263fae2f067f
Sam Weeks <weekss@google.com>
2020-10-24 16:16:48 +0100

    secret message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          bye there Hi there bye therereeeeeee!
+          Hi there, secret message!
         </p>
         <a
           className="App-link"
